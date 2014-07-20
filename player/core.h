Su13n/mpv

commit 1301a907617459237fb0071b4640ad53d0ae491f

demux: add a demuxer thread
This adds a thread to the demuxer which reads packets asynchronously.
It will do so until a configurable minimum packet queue size is
reached. (See options.rst additions.)

For now, the thread is disabled by default. There are some corner cases
that have to be fixed, such as fixing cache behavior with webradios.

Note that most interaction with the demuxer is still blocking, so if
e.g. network dies, the player will still freeze. But this change will
make it possible to remove most causes for freezing.

Most of the new code in demux.c actually consists of weird caches to
compensate for thread-safety issues (with the previously single-threaded
design), or to avoid blocking by having to wait on the demuxer thread.

Most of the changes in the player are due to the fact that we must not
access the source stream directly. the demuxer thread already accesses
it, and the stream stuff is not thread-safe.

For timeline stuff (like ordered chapters), we enable the thread for the
current segment only. We also clear its packet queue on seek, so that
the remaining (unconsumed) readahead buffer doesn't waste memory.

Keep in mind that insane subtitles (such as ASS typesetting muxed into
mkv files) will practically disable the readahead, because the total
queue size is considered when checking whether the minimum queue size
was reached.

player/core.h
@@ -292,7 +292,6 @@ typedef struct MPContext {
     double audio_delay;
 
     double last_heartbeat;
-    double last_metadata_update;
     double last_idle_tick;
 
     double mouse_timer;
@@ -401,6 +400,7 @@ struct playlist_entry *mp_next_file(struct MPContext *mpctx, int direction,
                                     bool force);
 void mp_set_playlist_entry(struct MPContext *mpctx, struct playlist_entry *e);
 void mp_play_files(struct MPContext *mpctx);
+void update_demuxer_properties(struct MPContext *mpctx);
 
 // main.c
 int mpv_main(int argc, char *argv[]);

player/loadfile.c
@@ -221,10 +221,15 @@ static void print_stream(struct MPContext *mpctx, struct track *t)
     MP_INFO(mpctx, "%s\n", b);
 }
 
-static void print_file_properties(struct MPContext *mpctx)
+void update_demuxer_properties(struct MPContext *mpctx)
 {
     struct demuxer *demuxer = mpctx->master_demuxer;
-    if (demuxer->num_editions > 1) {
+    if (!demuxer)
+        return;
+    demux_update(demuxer);
+    int events = demuxer->events;
+    demuxer->events = 0;
+    if ((events & DEMUX_EVENT_INIT) && demuxer->num_editions > 1) {
         for (int n = 0; n < demuxer->num_editions; n++) {
             struct demux_edition *edition = &demuxer->editions[n];
             char b[128] = {0};
@@ -238,10 +243,20 @@ static void print_file_properties(struct MPContext *mpctx)
             MP_INFO(mpctx, "%s\n", b);
         }
     }
-    for (int t = 0; t < STREAM_TYPE_COUNT; t++) {
-        for (int n = 0; n < mpctx->num_tracks; n++)
-            if (mpctx->tracks[n]->type == t)
-                print_stream(mpctx, mpctx->tracks[n]);
+    if (events & DEMUX_EVENT_STREAMS) {
+        add_demuxer_tracks(mpctx, demuxer);
+        for (int t = 0; t < STREAM_TYPE_COUNT; t++) {
+            for (int n = 0; n < mpctx->num_tracks; n++)
+                if (mpctx->tracks[n]->type == t)
+                    print_stream(mpctx, mpctx->tracks[n]);
+        }
+    }
+    struct mp_tags *info = demuxer->metadata;
+    if ((events & DEMUX_EVENT_METADATA) && info->num_keys) {
+        MP_INFO(mpctx, "File tags:\n");
+        for (int n = 0; n < info->num_keys; n++)
+            MP_INFO(mpctx, " %s: %s\n", info->keys[n], info->values[n]);
+        mp_notify(mpctx, MPV_EVENT_METADATA_UPDATE, NULL);
     }
 }
 
@@ -311,6 +326,11 @@ bool timeline_set_part(struct MPContext *mpctx, int i, bool force)
     uninit_player(mpctx, INITIALIZED_VCODEC | (mpctx->opts->fixed_vo ? 0 : INITIALIZED_VO) | (mpctx->opts->gapless_audio ? 0 : INITIALIZED_AO) | INITIALIZED_ACODEC | INITIALIZED_SUB | INITIALIZED_SUB2);
     mpctx->stop_play = orig_stop_play;
 
+    if (mpctx->demuxer) {
+        demux_stop_thread(mpctx->demuxer);
+        demux_flush(mpctx->demuxer);
+    }
+
     mpctx->demuxer = n->source;
     mpctx->stream = mpctx->demuxer->stream;
 
@@ -332,6 +352,9 @@ bool timeline_set_part(struct MPContext *mpctx, int i, bool force)
         }
     }
     reselect_demux_streams(mpctx);
+
+    if (mpctx->demuxer && mpctx->opts->demuxer_thread)
+        demux_start_thread(mpctx->demuxer);
 
     return true;
 }
@@ -364,6 +387,7 @@ static int find_new_tid(struct MPContext *mpctx, enum stream_type t)
 }
 
 static struct track *add_stream_track(struct MPContext *mpctx,
+                                      struct demuxer *demuxer,
                                       struct sh_stream *stream,
                                       bool under_timeline)
 {
@@ -383,7 +407,7 @@ static struct track *add_stream_track(struct MPContext *mpctx,
         .attached_picture = stream->attached_picture != NULL,
         .lang = stream->lang,
         .under_timeline = under_timeline,
-        .demuxer = stream->demuxer,
+        .demuxer = demuxer,
         .stream = stream,
     };
     MP_TARRAY_APPEND(mpctx, mpctx->tracks, mpctx->num_tracks, track);
@@ -398,7 +422,7 @@ static struct track *add_stream_track(struct MPContext *mpctx,
 void add_demuxer_tracks(struct MPContext *mpctx, struct demuxer *demuxer)
 {
     for (int n = 0; n < demuxer->num_streams; n++)
-        add_stream_track(mpctx, demuxer->streams[n], !!mpctx->timeline);
+        add_stream_track(mpctx, demuxer, demuxer->streams[n], !!mpctx->timeline);
 }
 
 // Result numerically higher => better match. 0 == no match.
@@ -651,9 +675,11 @@ static void open_subtitles_from_options(struct MPContext *mpctx)
         void *tmp = talloc_new(NULL);
         char *base_filename = mpctx->filename;
         char *stream_filename = NULL;
-        if (stream_control(mpctx->stream, STREAM_CTRL_GET_BASE_FILENAME,
-                           &stream_filename) > 0)
-            base_filename = talloc_steal(tmp, stream_filename);
+        if (mpctx->demuxer) {
+            if (demux_stream_control(mpctx->demuxer, STREAM_CTRL_GET_BASE_FILENAME,
+                                     &stream_filename) > 0)
+                base_filename = talloc_steal(tmp, stream_filename);
+        }
         struct subfn *list = find_text_subtitles(mpctx->global, base_filename);
         talloc_steal(tmp, list);
         for (int i = 0; list && list[i].fname; i++) {
@@ -703,7 +729,7 @@ static struct track *open_external_file(struct MPContext *mpctx, char *filename,
     for (int n = 0; n < demuxer->num_streams; n++) {
         struct sh_stream *sh = demuxer->streams[n];
         if (sh->type == filter) {
-            struct track *t = add_stream_track(mpctx, sh, false);
+            struct track *t = add_stream_track(mpctx, demuxer, sh, false);
             t->is_external = true;
             t->title = talloc_strdup(t, disp_filename);
             t->external_filename = talloc_strdup(t, filename);
@@ -1160,15 +1186,17 @@ goto_reopen_demuxer: ;
     }
     reselect_demux_streams(mpctx);
 
+    update_demuxer_properties(mpctx);
+
+    if (mpctx->demuxer && opts->demuxer_thread)
+        demux_start_thread(mpctx->demuxer);
+
     if (mpctx->current_track[0][STREAM_VIDEO] &&
         mpctx->current_track[0][STREAM_VIDEO]->attached_picture)
     {
         MP_INFO(mpctx,
             "Displaying attached picture. Use --no-audio-display to prevent this.\n");
     }
-
-    demux_info_update(mpctx->master_demuxer);
-    print_file_properties(mpctx);
 
 #if HAVE_ENCODING
     if (mpctx->encode_lavc_ctx && mpctx->current_track[0][STREAM_VIDEO])
@@ -1189,11 +1217,11 @@ goto_reopen_demuxer: ;
     //==================== START PLAYING =======================
 
     if (!mpctx->d_video && !mpctx->d_audio) {
-        struct stream *s = mpctx->stream;
+        struct demuxer *d = mpctx->demuxer;
         MP_FATAL(mpctx, "No video or audio streams selected.\n");
-        if (s->uncached_type == STREAMTYPE_DVB) {
+        if (d->stream->uncached_type == STREAMTYPE_DVB) {
             int dir = mpctx->last_dvb_step;
-            if (stream_control(s, STREAM_CTRL_DVB_STEP_CHANNEL, &dir) > 0)
+            if (demux_stream_control(d, STREAM_CTRL_DVB_STEP_CHANNEL, &dir) > 0)
                 mpctx->stop_play = PT_RELOAD_DEMUXER;
         }
         goto terminate_playback;

demux/demux.h
@@ -31,10 +31,15 @@
 #include "packet.h"
 #include "stheader.h"
 
-struct MPOpts;
-
-#define MAX_PACKS 4096
-#define MAX_PACK_BYTES 0x8000000  // 128 MiB
+// Maximum total size of packets queued - if larger, no new packets are read,
+// and the demuxer pretends EOF was reached.
+#define MAX_PACKS 16000
+#define MAX_PACK_BYTES (400 * 1024 * 1024)
+// Minimum total size of packets queued - the demuxer thread will read more
+// packets, until either number or total size of the packets exceed the minimum.
+// This can actually be configured with command line options.
+#define MIN_PACKS 64
+#define MIN_PACK_BYTES (5 * 1024 * 1024)
 
 enum demuxer_type {
     DEMUXER_TYPE_GENERIC = 0,
@@ -48,14 +53,14 @@ enum demuxer_type {
 #define DEMUXER_CTRL_NOTIMPL -1
 #define DEMUXER_CTRL_DONTKNOW 0
 #define DEMUXER_CTRL_OK 1
-#define DEMUXER_CTRL_GUESS 2
 
 enum demux_ctrl {
     DEMUXER_CTRL_SWITCHED_TRACKS = 1,
     DEMUXER_CTRL_GET_TIME_LENGTH,
     DEMUXER_CTRL_RESYNC,
     DEMUXER_CTRL_IDENTIFY_PROGRAM,
-    DEMUXER_CTRL_STREAM_CTRL,       // stupid workaround for legacy TV code
+    DEMUXER_CTRL_STREAM_CTRL,
+    DEMUXER_CTRL_STREAM_AUTOSELECT,
 };
 
 struct demux_ctrl_stream_ctrl {
@@ -87,7 +92,10 @@ enum demux_check {
 };
 
 enum demux_event {
-    DEMUX_EVENT_METADATA = (1 << 0),
+    DEMUX_EVENT_INIT = 1 << 0,      // complete (re-)initialization
+    DEMUX_EVENT_STREAMS = 1 << 1,   // a stream was added
+    DEMUX_EVENT_METADATA = 1 << 2,  // metadata or stream_metadata changed
+    DEMUX_EVENT_ALL = 0xFFFF,
 };
 
 #define MAX_SH_STREAMS 256
@@ -172,15 +180,12 @@ typedef struct demuxer {
     const demuxer_desc_t *desc; ///< Demuxer description structure
     const char *filetype; // format name when not identified by demuxer (libavformat)
     int64_t filepos;  // input stream current pos.
-    struct stream *stream;
     char *filename;  // same as stream->url
     enum demuxer_type type;
     int seekable; // flag
     double start_time;
     // File format allows PTS resets (even if the current file is without)
     bool ts_resets_possible;
-    bool warned_queue_overflow;
-    bool stream_select_default; // initial selection status of a new stream
 
     // Bitmask of DEMUX_EVENT_*
     int events;
@@ -207,13 +212,19 @@ typedef struct demuxer {
 
     struct mp_tags *metadata;
 
-    struct mp_tags *stream_metadata;
-
     void *priv;   // demuxer-specific internal data
     struct MPOpts *opts;
     struct mpv_global *global;
     struct mp_log *log, *glog;
     struct demuxer_params *params;
+
+    struct demux_internal *in; // internal to demux.c
+
+    // Since the demuxer can run in its own thread, and the stream is not
+    // thread-safe, only the demuxer is allowed to access the stream directly.
+    // You can freely use demux_stream_control() to send STREAM_CTRLs, or use
+    // demux_pause() to get exclusive access to the stream.
+    struct stream *stream;
 } demuxer_t;
 
 typedef struct {
@@ -238,11 +249,14 @@ struct demuxer *demux_open(struct stream *stream, char *force_format,
                            struct demuxer_params *params,
                            struct mpv_global *global);
 
+void demux_start_thread(struct demuxer *demuxer);
+void demux_stop_thread(struct demuxer *demuxer);
+void demux_set_wakeup_cb(struct demuxer *demuxer, void (*cb)(void *ctx), void *ctx);
+
 void demux_flush(struct demuxer *demuxer);
 int demux_seek(struct demuxer *demuxer, float rel_seek_secs, int flags);
 
 char *demux_info_get(struct demuxer *demuxer, const char *opt);
-bool demux_info_update(struct demuxer *demuxer);
 
 int demux_control(struct demuxer *demuxer, int cmd, void *arg);
 
@@ -250,6 +264,7 @@ void demuxer_switch_track(struct demuxer *demuxer, enum stream_type type,
                           struct sh_stream *stream);
 void demuxer_select_track(struct demuxer *demuxer, struct sh_stream *stream,
                           bool selected);
+void demux_set_stream_autoselect(struct demuxer *demuxer, bool autoselect);
 
 void demuxer_help(struct mp_log *log);
 
@@ -259,6 +274,14 @@ int demuxer_add_chapter(struct demuxer *demuxer, struct bstr name,
                         uint64_t start, uint64_t end, uint64_t demuxer_id);
 
 double demuxer_get_time_length(struct demuxer *demuxer);
+
+int demux_stream_control(demuxer_t *demuxer, int ctrl, void *arg);
+
+void demux_pause(demuxer_t *demuxer);
+void demux_unpause(demuxer_t *demuxer);
+
+void demux_changed(demuxer_t *demuxer, int events);
+void demux_update(demuxer_t *demuxer);
 
 struct sh_stream *demuxer_stream_by_demuxer_id(struct demuxer *d,
                                                enum stream_type t, int id);

demux/demux_disc.c
@@ -205,6 +205,8 @@ static int d_fill_buffer(demuxer_t *demuxer)
     if (!pkt)
         return 0;
 
+    demux_update(p->slave);
+
     if (p->seek_reinit)
         reset_pts(demuxer);
 
@@ -285,6 +287,13 @@ static int d_open(demuxer_t *demuxer, enum demux_check check)
     if (demuxer->stream->uncached_type == STREAMTYPE_CDDA)
         demux = "+rawaudio";
 
+    char *t = NULL;
+    stream_control(demuxer->stream, STREAM_CTRL_GET_DISC_NAME, &t);
+    if (t) {
+        mp_tags_set_bstr(demuxer->metadata, bstr0("TITLE"), bstr0(t));
+        talloc_free(t);
+    }
+
     // Initialize the playback time. We need to read _some_ data to get the
     // correct stream-layer time (at least with libdvdnav).
     stream_peek(demuxer->stream, 1);
@@ -295,7 +304,7 @@ static int d_open(demuxer_t *demuxer, enum demux_check check)
         return -1;
 
     // So that we don't miss initial packets of delayed subtitle streams.
-    p->slave->stream_select_default = true;
+    demux_set_stream_autoselect(p->slave, true);
 
     // Can be seekable even if the stream isn't.
     demuxer->seekable = true;

demux/demux_lavf.c
@@ -591,6 +591,7 @@ static void handle_stream(demuxer_t *demuxer, int i)
     }
 
     select_tracks(demuxer, i);
+    demux_changed(demuxer, DEMUX_EVENT_STREAMS);
 }
 
 // Add any new streams that might have been added
@@ -615,7 +616,7 @@ static void update_metadata(demuxer_t *demuxer, AVPacket *pkt)
             mp_tags_clear(demuxer->metadata);
             mp_tags_copy_from_av_dictionary(demuxer->metadata, dict);
             av_dict_free(&dict);
-            demuxer->events |= DEMUX_EVENT_METADATA;
+            demux_changed(demuxer, DEMUX_EVENT_METADATA);
         }
     }
 #endif

demux/stheader.h
@@ -31,7 +31,6 @@ struct demuxer;
 
 struct sh_stream {
     enum stream_type type;
-    struct demuxer *demuxer;
     // Index into demuxer->streams.
     int index;
     // Demuxer/format specific ID. Corresponds to the stream IDs as encoded in

options/options.c
@@ -214,6 +214,9 @@ const m_option_t mp_opts[] = {
     OPT_STRING("demuxer", demuxer_name, 0),
     OPT_STRING("audio-demuxer", audio_demuxer_name, 0),
     OPT_STRING("sub-demuxer", sub_demuxer_name, 0),
+    OPT_FLAG("demuxer-thread", demuxer_thread, 0),
+    OPT_INTRANGE("demuxer-readahead-packets", demuxer_min_packs, 0, 0, MAX_PACKS),
+    OPT_INTRANGE("demuxer-readahead-bytes", demuxer_min_bytes, 0, 0, MAX_PACK_BYTES),
 
     OPT_DOUBLE("mf-fps", mf_fps, 0),
     OPT_STRING("mf-type", mf_type, 0),
@@ -591,6 +594,9 @@ const struct MPOpts mp_default_opts = {
     },
     .stream_cache_pause = 50,
     .stream_cache_unpause = 100,
+    .demuxer_thread = 0,
+    .demuxer_min_packs = MIN_PACKS,
+    .demuxer_min_bytes = MIN_PACK_BYTES,
     .network_rtsp_transport = 2,
     .chapterrange = {-1, -1},
     .edition_id = -1,

commit 259c0c1eda50a55b79930e1fbc17bbe91a4d698c

player: fix regression with ordered chapters
Broken by commit 1301a907. This commit added demuxer threading, and
changed some other things to make them simpler and more orthogonal. One
of these things was ntofications about streams that appear during
playback. That's an obscure corner case, but the change made handling of
it as natural as normal initialization.

This didn't work for two reasons:
1. When playing an ordered chapters file where the initial segment was
not from the main file, its streams were added to the track list. So
they were printed twice, and switching to the next segment didn't work,
because the right streams were not selected.
2. EDL, CUE, as well as possibly certain Matroska files don't have any
data or tracks in the "main" demuxer, so normally the first segment is
picked for the track list. This was simply broken.

Fix by sprinkling the code with various hacks.

player/core.h
@@ -125,6 +125,9 @@ struct track {
     // If the track's stream changes with the timeline (ordered chapters).
     bool under_timeline;
 
+    // Does not change with under_timeline, but it useless for most purposes.
+    struct sh_stream *original_stream;
+
     // Value can change if under_timeline==true.
     struct demuxer *demuxer;
     // Invariant: !stream || stream->demuxer == demuxer
@@ -221,6 +224,7 @@ typedef struct MPContext {
     // Uses: accessing metadata (consider ordered chapters case, where the main
     // demuxer defines metadata), or special purpose demuxers like TV.
     struct demuxer *master_demuxer;
+    struct demuxer *track_layout;   // complication for ordered chapters
 
     struct mixer *mixer;
     struct ao *ao;

player/loadfile.c
@@ -228,7 +228,6 @@ void update_demuxer_properties(struct MPContext *mpctx)
         return;
     demux_update(demuxer);
     int events = demuxer->events;
-    demuxer->events = 0;
     if ((events & DEMUX_EVENT_INIT) && demuxer->num_editions > 1) {
         for (int n = 0; n < demuxer->num_editions; n++) {
             struct demux_edition *edition = &demuxer->editions[n];
@@ -243,13 +242,15 @@ void update_demuxer_properties(struct MPContext *mpctx)
             MP_INFO(mpctx, "%s\n", b);
         }
     }
-    if (events & DEMUX_EVENT_STREAMS) {
-        add_demuxer_tracks(mpctx, demuxer);
+    struct demuxer *tracks = mpctx->track_layout;
+    if (tracks->events & DEMUX_EVENT_STREAMS) {
+        add_demuxer_tracks(mpctx, tracks);
         for (int t = 0; t < STREAM_TYPE_COUNT; t++) {
             for (int n = 0; n < mpctx->num_tracks; n++)
                 if (mpctx->tracks[n]->type == t)
                     print_stream(mpctx, mpctx->tracks[n]);
         }
+        tracks->events &= ~DEMUX_EVENT_STREAMS;
     }
     struct mp_tags *info = demuxer->metadata;
     if ((events & DEMUX_EVENT_METADATA) && info->num_keys) {
@@ -258,6 +259,7 @@ void update_demuxer_properties(struct MPContext *mpctx)
             MP_INFO(mpctx, " %s: %s\n", info->keys[n], info->values[n]);
         mp_notify(mpctx, MPV_EVENT_METADATA_UPDATE, NULL);
     }
+    demuxer->events = 0;
 }
 
 // Enable needed streams, disable others.
@@ -341,11 +343,6 @@ bool timeline_set_part(struct MPContext *mpctx, int i, bool force)
     uninit_player(mpctx, INITIALIZED_VCODEC | (mpctx->opts->fixed_vo ? 0 : INITIALIZED_VO) | (mpctx->opts->gapless_audio ? 0 : INITIALIZED_AO) | INITIALIZED_ACODEC | INITIALIZED_SUB | INITIALIZED_SUB2);
     mpctx->stop_play = orig_stop_play;
 
-    if (mpctx->demuxer) {
-        demux_stop_thread(mpctx->demuxer);
-        demux_flush(mpctx->demuxer);
-    }
-
     mpctx->demuxer = n->source;
     mpctx->stream = mpctx->demuxer->stream;
 
@@ -407,7 +404,7 @@ static struct track *add_stream_track(struct MPContext *mpctx,
 {
     for (int i = 0; i < mpctx->num_tracks; i++) {
         struct track *track = mpctx->tracks[i];
-        if (track->stream == stream)
+        if (track->original_stream == stream)
             return track;
     }
 
@@ -423,6 +420,7 @@ static struct track *add_stream_track(struct MPContext *mpctx,
         .under_timeline = under_timeline,
         .demuxer = demuxer,
         .stream = stream,
+        .original_stream = stream,
     };
     MP_TARRAY_APPEND(mpctx, mpctx->tracks, mpctx->num_tracks, track);
 
@@ -1146,18 +1144,20 @@ goto_reopen_demuxer: ;
     print_timeline(mpctx);
     load_chapters(mpctx);
 
+    mpctx->track_layout = mpctx->demuxer;
     if (mpctx->timeline) {
         // With Matroska, the "master" file usually dictates track layout etc.
         // On the contrary, the EDL and CUE demuxers are empty wrappers, as
         // well as Matroska ordered chapter playlist-like files.
+        mpctx->track_layout = mpctx->timeline[0].source;
         for (int n = 0; n < mpctx->num_timeline_parts; n++) {
-            if (mpctx->timeline[n].source == mpctx->demuxer)
-                goto main_is_ok;
+            if (mpctx->timeline[n].source == mpctx->demuxer) {
+                mpctx->track_layout = mpctx->demuxer;
+                break;
+            }
         }
-        mpctx->demuxer = mpctx->timeline[0].source;
-    main_is_ok: ;
     }
-    add_demuxer_tracks(mpctx, mpctx->demuxer);
+    add_demuxer_tracks(mpctx, mpctx->track_layout);
 
     mpctx->timeline_part = 0;
     if (mpctx->timeline)