Su13n/mpv

commit e5d3180889ab14a57aacb9772ee67ad24478a3f1

charset_conv: use our own UTF-8 check with ENCA only
Some charsets can look like valid UTF-8, but aren't UTF-8. One example
is ISO-2022-JP. While ENCA apparently likes to get misdetect real UTF-8,
this is not the case with uchardet. uchardet can detect ISO-2022-JP
correctly, but didn't even get to try, because our own UTF-8 check
succeeded. So run the UTF-8 check when using ENCA only.

Fixes #2195.

misc/charset_conv.c
@@ -107,6 +107,11 @@ static const char *ms_bom_guess(bstr buf)
 #if HAVE_ENCA
 static const char *enca_guess(struct mp_log *log, bstr buf, const char *language)
 {
+    // Do our own UTF-8 detection, because ENCA seems to get it wrong sometimes
+    // (suggested by divVerent). Explicitly allow cut-off UTF-8.
+    if (bstr_validate_utf8(buf) > -8)
+        return "UTF-8";
+
     if (!language || !language[0])
         language = "__"; // neutral language
 
@@ -201,12 +206,6 @@ const char *mp_charset_guess(void *talloc_ctx, struct mp_log *log, bstr buf,
         user_cp = "UTF-8:UTF-8-BROKEN";
 #endif
     }
-
-    // Do our own UTF-8 detection, because at least ENCA seems to get it
-    // wrong sometimes (suggested by divVerent).
-    int r = bstr_validate_utf8(buf);
-    if (r >= 0 || (r > -8 && (flags & MP_ICONV_ALLOW_CUTOFF)))
-        return "UTF-8";
 
     bstr params[3] = {{0}};
     split_colon(user_cp, 3, params);

commit 5e4b6e5f062a3a6ec4bf3df9b9567aab9299970e

sub: fix --sub-codepage UTF-8 with fallback
Fixes e.g --sub-codepage=utf8:gb18030 if the subtitle us UTF-8.

This was broken in commit e5d31808.

Also log the detected charset in verbose mode.

misc/charset_conv.c
@@ -239,6 +239,9 @@ const char *mp_charset_guess(void *talloc_ctx, struct mp_log *log, bstr buf,
     if (bstrcasecmp0(type, "utf8") == 0 || bstrcasecmp0(type, "utf-8") == 0) {
         if (!fallback)
             fallback = params[1].start; // must be already 0-terminated
+        int r = bstr_validate_utf8(buf);
+        if (r >= 0 || (r > -8 && (flags & MP_ICONV_ALLOW_CUTOFF)))
+            res = "utf-8";
     }
 
     if (res) {
@@ -252,6 +255,7 @@ const char *mp_charset_guess(void *talloc_ctx, struct mp_log *log, bstr buf,
     if (!res && !(flags & MP_STRICT_UTF8))
         res = "UTF-8-BROKEN";
 
+    mp_verbose(log, "Using charset '%s'.\n", res);
     return res;
 }
 