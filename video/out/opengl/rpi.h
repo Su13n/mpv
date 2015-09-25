Su13n/mpv

commit d47dff3faa246256d9e29daa215db4b5aedcb3f2

vo_opengl: load certain EGL extensions needed for VAAPI EGL interop
These extensions use a bunch of EGL types, so we need to include the EGL
headers in common.h to use our GL function loader with this.

In the future, we should probably require presence of the EGL headers to
reduce the hacks. This might be not so simple at least with OSX, so for
now this has to do.

wscript
@@ -609,6 +609,11 @@ video_output_features = [
         'groups': [ 'gl' ],
         'func': check_pkg_config('egl', 'gl'),
     } , {
+        'name': 'egl',
+        'desc': 'EGL',
+        'deps': [ 'egl-x11' ],
+        'func': check_true,
+    } , {
         'name': '--gl-wayland',
         'desc': 'OpenGL Wayland Backend',
         'deps': [ 'wayland' ],

TOOLS/old-configure
@@ -690,6 +690,7 @@ fi
 check_yes_no $_gl GL
 check_yes_no $_gl_x11 GL_X11
 check_yes_no $_gl_x11_egl EGL_X11
+check_yes_no $_gl_x11_egl EGL
 check_yes_no $_gl_wayland GL_WAYLAND
 echores "$_gl"
 

video/out/opengl/common.c
@@ -72,6 +72,7 @@ static bool check_ext(GL *gl, const char *name)
 #define FN_OFFS(name) offsetof(GL, name)
 
 #define DEF_FN(name)            {FN_OFFS(name), "gl" # name}
+#define DEF_FN_EGL(name)        {FN_OFFS(name), "egl" # name}
 #define DEF_FN_NAME(name, str)  {FN_OFFS(name), str}
 
 struct gl_function {
@@ -277,6 +278,22 @@ static const struct gl_functions gl_functions[] = {
     {
         .extension = "GL_APPLE_rgb_422",
         .provides = MPGL_CAP_APPLE_RGB_422,
+    },
+    // EGL extensions for vaapi EGL interop
+    {
+        .extension = "EGL_KHR_image_base",
+        .functions = (const struct gl_function[]) {
+            DEF_FN_EGL(CreateImageKHR),
+            DEF_FN_EGL(DestroyImageKHR),
+            {0}
+        },
+    },
+    {
+        .extension = "GL_OES_EGL_image",
+        .functions = (const struct gl_function[]) {
+            DEF_FN(EGLImageTargetTexture2DOES),
+            {0}
+        },
     },
     {
         .ver_core = 430,

video/out/opengl/common.h
@@ -46,6 +46,11 @@
 #include <GL/glext.h>
 #endif
 
+#if HAVE_EGL
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#endif
+
 #define MP_GET_GL_WORKAROUNDS
 #include "header_fixes.h"
 
@@ -267,6 +272,12 @@ struct GL {
 
     GLint (GLAPIENTRY *GetVideoSync)(GLuint *);
     GLint (GLAPIENTRY *WaitVideoSync)(GLint, GLint, unsigned int *);
+
+    EGLImageKHR (EGLAPIENTRY *CreateImageKHR)(EGLDisplay, EGLContext,
+                                              EGLenum, EGLClientBuffer,
+                                              const EGLint *);
+    EGLBoolean (EGLAPIENTRY *DestroyImageKHR)(EGLDisplay, EGLImageKHR);
+    void (EGLAPIENTRY *EGLImageTargetTexture2DOES)(GLenum, GLeglImageOES);
 
     void (GLAPIENTRY *DebugMessageCallback)(MP_GLDEBUGPROC callback,
                                             const void *userParam);

video/out/opengl/header_fixes.h
@@ -60,6 +60,25 @@
 #define GLvdpauSurfaceNV GLintptr
 #endif
 
+typedef void *MP_voidptr;
+
+#ifndef GL_OES_EGL_image
+#define GLeglImageOES MP_voidptr
+#endif
+#ifndef EGL_KHR_image
+#define EGLImageKHR MP_voidptr
+#endif
+
+#if !HAVE_EGL
+#define EGLBoolean unsigned int
+#define EGLenum unsigned int
+#define EGLint int
+#define EGLContext MP_voidptr
+#define EGLDisplay MP_voidptr
+#define EGLClientBuffer MP_voidptr
+#define EGLAPIENTRY GLAPIENTRY
+#endif
+
 #ifndef GL_DEBUG_SEVERITY_HIGH
 #define GL_DEBUG_SEVERITY_HIGH            0x9146
 #define GL_DEBUG_SEVERITY_MEDIUM          0x9147

video/out/opengl/x11egl.c
@@ -134,8 +134,10 @@ static bool config_window_x11_egl(struct MPGLContext *ctx, int flags)
         return false;
     }
 
+    const char *egl_exts = eglQueryString(p->egl_display, EGL_EXTENSIONS);
+
     void *(*gpa)(const GLubyte*) = (void *(*)(const GLubyte*))eglGetProcAddress;
-    mpgl_load_functions(ctx->gl, gpa, NULL, vo->log);
+    mpgl_load_functions(ctx->gl, gpa, egl_exts, vo->log);
 
     return true;
 }

commit 6b3ae6e152e5a3fa0bb083a947a4bfaeff3074df

vo_rpi, wayland: fix build
Broken by commit d47dff3f. If something is going to include EGL.h,
header_fixes.h has to know. This definitely affected vo_rpi, and
probably affects wayland builds (with x11egl didabled) as well.

video/out/opengl/rpi.h
@@ -1,6 +1,6 @@
 #include <bcm_host.h>
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
+
+#include "common.h"
 
 struct mp_egl_rpi {
     struct mp_log *log;

wscript
@@ -613,11 +613,6 @@ video_output_features = [
         'groups': [ 'gl' ],
         'func': check_pkg_config('egl', 'gl'),
     } , {
-        'name': 'egl',
-        'desc': 'EGL',
-        'deps': [ 'egl-x11' ],
-        'func': check_true,
-    } , {
         'name': '--gl-wayland',
         'desc': 'OpenGL Wayland Backend',
         'deps': [ 'wayland' ],
@@ -708,6 +703,11 @@ video_output_features = [
         'desc': 'OpenGL video outputs',
         'deps_any': [ 'gl-cocoa', 'gl-x11', 'gl-win32', 'gl-wayland', 'rpi' ],
         'func': check_true
+    } , {
+        'name': 'egl',
+        'desc': 'EGL',
+        'deps_any': [ 'egl-x11', 'rpi' , 'gl-wayland' ],
+        'func': check_true,
     }
 ]
 