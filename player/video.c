Su13n/mpv

commit f1d161d55f458cf47e63d1ab9ddf08859019dd8a

player: make --lavfi-complex changeable at runtime
Tends to be somewhat glitchy if subtitles are enabled, and you enable
and disable tracks.

On error, this will disable --lavfi-complex, which will result in
whatever behavior.

player/video.c
@@ -456,23 +456,20 @@ err_out:
     return 0;
 }
 
-int reinit_video_chain(struct MPContext *mpctx)
+void reinit_video_chain(struct MPContext *mpctx)
 {
-    return reinit_video_chain_src(mpctx, NULL);
+    struct track *track = mpctx->current_track[0][STREAM_VIDEO];
+    if (!track || !track->stream) {
+        error_on_track(mpctx, track);
+        handle_force_window(mpctx, true);
+        return;
+    }
+    reinit_video_chain_src(mpctx, track);
 }
 
-int reinit_video_chain_src(struct MPContext *mpctx, struct lavfi_pad *src)
+// (track=NULL creates a blank chain, used for lavfi-complex)
+void reinit_video_chain_src(struct MPContext *mpctx, struct track *track)
 {
-    struct track *track = NULL;
-    struct sh_stream *sh = NULL;
-    if (!src) {
-        track = mpctx->current_track[0][STREAM_VIDEO];
-        if (!track)
-            return 0;
-        sh = track->stream;
-        if (!sh)
-            goto no_video;
-    }
     assert(!mpctx->vo_chain);
 
     if (!mpctx->video_out) {
@@ -504,11 +501,7 @@ int reinit_video_chain_src(struct MPContext *mpctx, struct lavfi_pad *src)
 
     vo_c->hwdec_devs = vo_c->vo->hwdec_devs;
 
-    if (mpctx->lavfi)
-        lavfi_set_hwdec_devs(mpctx->lavfi, vo_c->hwdec_devs);
-
-    vo_c->filter_src = src;
-    if (!vo_c->filter_src) {
+    if (track) {
         vo_c->track = track;
         track->vo_c = vo_c;
         if (!init_video_decoder(mpctx, track))
@@ -516,7 +509,7 @@ int reinit_video_chain_src(struct MPContext *mpctx, struct lavfi_pad *src)
 
         vo_c->video_src = track->d_video;
         vo_c->container_fps = vo_c->video_src->fps;
-        vo_c->is_coverart = !!sh->attached_picture;
+        vo_c->is_coverart = !!track->stream->attached_picture;
 
         track->vo_c = vo_c;
         vo_c->track = track;
@@ -540,14 +533,12 @@ int reinit_video_chain_src(struct MPContext *mpctx, struct lavfi_pad *src)
     reset_video_state(mpctx);
     reset_subtitle_state(mpctx);
 
-    return 1;
+    return;
 
 err_out:
-no_video:
     uninit_video_chain(mpctx);
     error_on_track(mpctx, track);
     handle_force_window(mpctx, true);
-    return 0;
 }
 
 // Try to refresh the video by doing a precise seek to the currently displayed

options/m_option.h
@@ -408,7 +408,8 @@ struct m_option {
 #define UPDATE_PRIORITY         (1 << 15) // --priority (Windows-only)
 #define UPDATE_SCREENSAVER      (1 << 16) // --stop-screensaver
 #define UPDATE_VOL              (1 << 17) // softvol related options
-#define UPDATE_OPT_LAST         (1 << 17)
+#define UPDATE_LAVFI_COMPLEX    (1 << 18) // --lavfi-complex
+#define UPDATE_OPT_LAST         (1 << 18)
 
 // All bits between _FIRST and _LAST (inclusive)
 #define UPDATE_OPTS_MASK \

options/options.c
@@ -362,7 +362,7 @@ const m_option_t mp_opts[] = {
     OPT_STRINGLIST("alang", stream_lang[STREAM_AUDIO], 0),
     OPT_STRINGLIST("slang", stream_lang[STREAM_SUB], 0),
 
-    OPT_STRING("lavfi-complex", lavfi_complex, 0),
+    OPT_STRING("lavfi-complex", lavfi_complex, UPDATE_LAVFI_COMPLEX),
 
     OPT_CHOICE("audio-display", audio_display, 0,
                ({"no", 0}, {"attachment", 1})),

player/audio.c
@@ -557,25 +557,19 @@ init_error:
 
 void reinit_audio_chain(struct MPContext *mpctx)
 {
-    reinit_audio_chain_src(mpctx, NULL);
+    struct track *track = NULL;
+    track = mpctx->current_track[0][STREAM_AUDIO];
+    if (!track || !track->stream) {
+        uninit_audio_out(mpctx);
+        error_on_track(mpctx, track);
+        return;
+    }
+    reinit_audio_chain_src(mpctx, track);
 }
 
-void reinit_audio_chain_src(struct MPContext *mpctx, struct lavfi_pad *src)
+// (track=NULL creates a blank chain, used for lavfi-complex)
+void reinit_audio_chain_src(struct MPContext *mpctx, struct track *track)
 {
-    struct track *track = NULL;
-    struct sh_stream *sh = NULL;
-    if (!src) {
-        track = mpctx->current_track[0][STREAM_AUDIO];
-        if (!track) {
-            uninit_audio_out(mpctx);
-            return;
-        }
-        sh = track->stream;
-        if (!sh) {
-            uninit_audio_out(mpctx);
-            goto no_audio;
-        }
-    }
     assert(!mpctx->ao_chain);
 
     mp_notify(mpctx, MPV_EVENT_AUDIO_RECONFIG, NULL);
@@ -584,15 +578,14 @@ void reinit_audio_chain_src(struct MPContext *mpctx, struct lavfi_pad *src)
     mpctx->ao_chain = ao_c;
     ao_c->log = mpctx->log;
     ao_c->af = af_new(mpctx->global);
-    if (sh)
-        ao_c->af->replaygain_data = sh->codec->replaygain_data;
+    if (track && track->stream)
+        ao_c->af->replaygain_data = track->stream->codec->replaygain_data;
     ao_c->spdif_passthrough = true;
     ao_c->pts = MP_NOPTS_VALUE;
     ao_c->ao_buffer = mp_audio_buffer_create(NULL);
     ao_c->ao = mpctx->ao;
 
-    ao_c->filter_src = src;
-    if (!ao_c->filter_src) {
+    if (track) {
         ao_c->track = track;
         track->ao_c = ao_c;
         if (!init_audio_decoder(mpctx, track))
@@ -614,7 +607,6 @@ void reinit_audio_chain_src(struct MPContext *mpctx, struct lavfi_pad *src)
 init_error:
     uninit_audio_chain(mpctx);
     uninit_audio_out(mpctx);
-no_audio:
     error_on_track(mpctx, track);
 }
 

player/command.c
@@ -5901,6 +5901,9 @@ void mp_option_change_callback(void *ctx, struct m_config_option *co, int flags)
 
     if (flags & UPDATE_VOL)
         audio_update_volume(mpctx);
+
+    if (flags & UPDATE_LAVFI_COMPLEX)
+        update_lavfi_complex(mpctx);
 }
 
 void mp_notify_property(struct MPContext *mpctx, const char *property)

player/core.h
@@ -367,6 +367,8 @@ typedef struct MPContext {
     double last_frame_duration;
     // Video PTS, or audio PTS if video has ended.
     double playback_pts;
+    // Last known "good" PTS
+    double canonical_pts;
     // audio stats only
     int64_t audio_stat_start;
     double written_audio;
@@ -478,7 +480,7 @@ void update_playback_speed(struct MPContext *mpctx);
 void uninit_audio_out(struct MPContext *mpctx);
 void uninit_audio_chain(struct MPContext *mpctx);
 int init_audio_decoder(struct MPContext *mpctx, struct track *track);
-void reinit_audio_chain_src(struct MPContext *mpctx, struct lavfi_pad *src);
+void reinit_audio_chain_src(struct MPContext *mpctx, struct track *track);
 void audio_update_volume(struct MPContext *mpctx);
 void audio_update_balance(struct MPContext *mpctx);
 void reload_audio_output(struct MPContext *mpctx);
@@ -522,6 +524,7 @@ void prefetch_next(struct MPContext *mpctx);
 void close_recorder(struct MPContext *mpctx);
 void close_recorder_and_error(struct MPContext *mpctx);
 void open_recorder(struct MPContext *mpctx, bool on_init);
+void update_lavfi_complex(struct MPContext *mpctx);
 
 // main.c
 int mp_initialize(struct MPContext *mpctx, char **argv);
@@ -611,8 +614,8 @@ int video_set_colors(struct vo_chain *vo_c, const char *item, int value);
 int video_vf_vo_control(struct vo_chain *vo_c, int vf_cmd, void *data);
 void reset_video_state(struct MPContext *mpctx);
 int init_video_decoder(struct MPContext *mpctx, struct track *track);
-int reinit_video_chain(struct MPContext *mpctx);
-int reinit_video_chain_src(struct MPContext *mpctx, struct lavfi_pad *src);
+void reinit_video_chain(struct MPContext *mpctx);
+void reinit_video_chain_src(struct MPContext *mpctx, struct track *track);
 int reinit_video_filters(struct MPContext *mpctx);
 void write_video(struct MPContext *mpctx);
 void mp_force_video_refresh(struct MPContext *mpctx);

player/lavfi.c
@@ -249,10 +249,17 @@ struct lavfi *lavfi_create(struct mp_log *log, char *graph_string)
 
 void lavfi_destroy(struct lavfi *c)
 {
+    if (!c)
+        return;
     free_graph(c);
     clear_data(c);
     av_frame_free(&c->tmp_frame);
     talloc_free(c);
+}
+
+const char *lavfi_get_graph(struct lavfi *c)
+{
+    return c->graph_string;
 }
 
 struct lavfi_pad *lavfi_find_pad(struct lavfi *c, char *name)
@@ -484,9 +491,11 @@ static void dump_graph(struct lavfi *c)
 #endif
 }
 
-void lavfi_set_hwdec_devs(struct lavfi *c, struct mp_hwdec_devices *hwdevs)
+void lavfi_pad_set_hwdec_devs(struct lavfi_pad *pad,
+                              struct mp_hwdec_devices *hwdevs)
 {
-    c->hwdec_devs = hwdevs;
+    // We don't actually treat this per-pad.
+    pad->main->hwdec_devs = hwdevs;
 }
 
 // Initialize the graph if all inputs have formats set. If it's already

player/lavfi.h
@@ -13,6 +13,7 @@ enum lavfi_direction {
 };
 
 struct lavfi *lavfi_create(struct mp_log *log, char *graph_string);
+const char *lavfi_get_graph(struct lavfi *c);
 void lavfi_destroy(struct lavfi *c);
 struct lavfi_pad *lavfi_find_pad(struct lavfi *c, char *name);
 enum lavfi_direction lavfi_pad_direction(struct lavfi_pad *pad);
@@ -22,7 +23,8 @@ bool lavfi_get_connected(struct lavfi_pad *pad);
 bool lavfi_process(struct lavfi *c);
 bool lavfi_has_failed(struct lavfi *c);
 void lavfi_seek_reset(struct lavfi *c);
-void lavfi_set_hwdec_devs(struct lavfi *c, struct mp_hwdec_devices *hwdevs);
+void lavfi_pad_set_hwdec_devs(struct lavfi_pad *pad,
+                              struct mp_hwdec_devices *hwdevs);
 int lavfi_request_frame_a(struct lavfi_pad *pad, struct mp_audio **out_aframe);
 int lavfi_request_frame_v(struct lavfi_pad *pad, struct mp_image **out_vframe);
 bool lavfi_needs_input(struct lavfi_pad *pad);

commit 4047f2d4c3761192a8925b1f03dbe3bb2538c0ce

player: do not destroy VO immediately if there is no video track
Commit f1d161d55f45 accidentally added the handle_force_window() call if
no track is selected.

This was OK, but breaks something like "mpv *", where some files are not
playable (like subtitle files) - the unplayable files would remove and
recreate the VO window, which is annoying.

Just drop the call again.

player/video.c
@@ -461,7 +461,6 @@ void reinit_video_chain(struct MPContext *mpctx)
     struct track *track = mpctx->current_track[0][STREAM_VIDEO];
     if (!track || !track->stream) {
         error_on_track(mpctx, track);
-        handle_force_window(mpctx, true);
         return;
     }
     reinit_video_chain_src(mpctx, track);