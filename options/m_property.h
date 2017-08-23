Su13n/mpv

commit 03cf150ff3516789d581214177f291d46310aaf4

video: redo video equalizer option handling
I really wouldn't care much about this, but some parts of the core code
are under HAVE_GPL, so there's some need to get rid of it. Simply turn
the video equalizer from its current fine-grained handling with vf/vo
fallbacks into global options. This makes updating them much simpler.

This removes any possibility of applying video equalizers in filters,
which affects vf_scale, and the previously removed vf_eq. Not a big
loss, since the preferred VOs have this builtin.

Remove video equalizer handling from vo_direct3d, vo_sdl, vo_vaapi, and
vo_xv. I'm not going to waste my time on these legacy VOs.

vo.eq_opts_cache exists _only_ to send a VOCTRL_SET_EQUALIZER, which
exists _only_ to trigger a redraw. This seems silly, but for now I feel
like this is less of a pain. The rest of the equalizer using code is
self-updating.

See commit 96b906a51d5 for how some video equalizer code was GPL only.
Some command line option names and ranges can probably be traced back to
a GPL only committer, but we don't consider these copyrightable.

player/command.c
@@ -2613,35 +2613,6 @@ static int mp_property_frame_count(void *ctx, struct m_property *prop,
     return m_property_int_ro(action, arg, frames);
 }
 
-#if HAVE_GPL
-static int mp_property_video_color(void *ctx, struct m_property *prop,
-                                   int action, void *arg)
-{
-    const char *name = prop->priv ? prop->priv : prop->name;
-    MPContext *mpctx = ctx;
-    if (!mpctx->vo_chain)
-        return mp_property_generic_option(mpctx, prop, action, arg);
-
-    switch (action) {
-    case M_PROPERTY_SET: {
-        if (video_set_colors(mpctx->vo_chain, name, *(int *) arg) <= 0)
-            return M_PROPERTY_UNAVAILABLE;
-        break;
-    }
-    case M_PROPERTY_GET:
-        if (video_get_colors(mpctx->vo_chain, name, (int *)arg) <= 0)
-            return M_PROPERTY_UNAVAILABLE;
-        // Write new value to option variable
-        mp_property_generic_option(mpctx, prop, M_PROPERTY_SET, arg);
-        return M_PROPERTY_OK;
-    case M_PROPERTY_GET_NEUTRAL:
-        *(int *)arg = 0;
-        return M_PROPERTY_OK;
-    }
-    return mp_property_generic_option(mpctx, prop, action, arg);
-}
-#endif
-
 /// Video codec tag (RO)
 static int mp_property_video_format(void *ctx, struct m_property *prop,
                                     int action, void *arg)
@@ -4041,15 +4012,6 @@ static const struct m_property mp_properties_base[] = {
     {"ontop", mp_property_ontop},
     {"border", mp_property_border},
     {"on-all-workspaces", mp_property_all_workspaces},
-#if HAVE_GPL
-    {"gamma", mp_property_video_color},
-    {"brightness", mp_property_video_color},
-    {"contrast", mp_property_video_color},
-    {"saturation", mp_property_video_color},
-    {"hue", mp_property_video_color},
-    {"video-output-levels", mp_property_video_color,
-     .priv = (void *)"output-levels"},
-#endif
     {"video-out-params", mp_property_vo_imgparams},
     {"video-dec-params", mp_property_dec_imgparams},
     {"video-params", mp_property_vd_imgparams},

options/options.c
@@ -572,15 +572,7 @@ const m_option_t mp_opts[] = {
                .deprecation_message = "use Lua scripting instead"),
     OPT_FLOAT("heartbeat-interval", heartbeat_interval, CONF_MIN, 0),
 
-#if HAVE_GPL
-    OPT_INTRANGE("brightness", gamma_brightness, 0, -100, 100),
-    OPT_INTRANGE("saturation", gamma_saturation, 0, -100, 100),
-    OPT_INTRANGE("contrast", gamma_contrast, 0, -100, 100),
-    OPT_INTRANGE("hue", gamma_hue, 0, -100, 100),
-    OPT_INTRANGE("gamma", gamma_gamma, 0, -100, 100),
-    OPT_CHOICE_C("video-output-levels", video_output_levels, 0,
-                 mp_csp_levels_names),
-#endif
+    OPT_SUBSTRUCT("", video_equalizer, mp_csp_equalizer_conf, 0),
 
     OPT_FLAG("use-filedir-conf", use_filedir_conf, 0),
     OPT_CHOICE("osd-level", osd_level, 0,
@@ -856,11 +848,6 @@ const struct MPOpts mp_default_opts = {
     .heartbeat_interval = 30.0,
     .stop_screensaver = 1,
     .cursor_autohide_delay = 1000,
-    .gamma_gamma = 0,
-    .gamma_brightness = 0,
-    .gamma_contrast = 0,
-    .gamma_saturation = 0,
-    .gamma_hue = 0,
     .video_osd = 1,
     .osd_level = 1,
     .osd_duration = 1000,

options/options.h
@@ -119,13 +119,7 @@ typedef struct MPOpts {
     char *media_title;
     int force_rgba_osd;
 
-    // ranges -100 - 100, 1000 if the vo default should be used
-    int gamma_gamma;
-    int gamma_brightness;
-    int gamma_contrast;
-    int gamma_saturation;
-    int gamma_hue;
-    int video_output_levels;
+    struct mp_csp_equalizer_opts *video_equalizer;
 
     int stop_screensaver;
     int cursor_autohide_delay;

player/video.c
@@ -70,37 +70,6 @@ static const char av_desync_help_text[] =
 "position will not match to the video (see A-V status field).\n"
 "\n";
 
-#if HAVE_GPL
-int video_set_colors(struct vo_chain *vo_c, const char *item, int value)
-{
-    vf_equalizer_t data;
-
-    data.item = item;
-    data.value = value;
-
-    MP_VERBOSE(vo_c, "set video colors %s=%d \n", item, value);
-    if (video_vf_vo_control(vo_c, VFCTRL_SET_EQUALIZER, &data) == CONTROL_TRUE)
-        return 1;
-    MP_VERBOSE(vo_c, "Video attribute '%s' is not supported by selected vo.\n",
-               item);
-    return 0;
-}
-
-int video_get_colors(struct vo_chain *vo_c, const char *item, int *value)
-{
-    vf_equalizer_t data;
-
-    data.item = item;
-
-    MP_VERBOSE(vo_c, "get video colors %s \n", item);
-    if (video_vf_vo_control(vo_c, VFCTRL_GET_EQUALIZER, &data) == CONTROL_TRUE) {
-        *value = data.value;
-        return 1;
-    }
-    return 0;
-}
-#endif
-
 // Send a VCTRL, or if it doesn't work, translate it to a VOCTRL and try the VO.
 int video_vf_vo_control(struct vo_chain *vo_c, int vf_cmd, void *data)
 {
@@ -110,26 +79,6 @@ int video_vf_vo_control(struct vo_chain *vo_c, int vf_cmd, void *data)
             return r;
     }
 
-    switch (vf_cmd) {
-    case VFCTRL_SET_EQUALIZER: {
-        vf_equalizer_t *eq = data;
-        if (!vo_c->vo->config_ok)
-            return CONTROL_FALSE;                       // vo not configured?
-        struct voctrl_set_equalizer_args param = {
-            eq->item, eq->value
-        };
-        return vo_control(vo_c->vo, VOCTRL_SET_EQUALIZER, &param) == VO_TRUE;
-    }
-    case VFCTRL_GET_EQUALIZER: {
-        vf_equalizer_t *eq = data;
-        if (!vo_c->vo->config_ok)
-            return CONTROL_FALSE;                       // vo not configured?
-        struct voctrl_get_equalizer_args param = {
-            eq->item, &eq->value
-        };
-        return vo_control(vo_c->vo, VOCTRL_GET_EQUALIZER, &param) == VO_TRUE;
-    }
-    }
     return CONTROL_UNKNOWN;
 }
 
@@ -980,28 +929,6 @@ static void update_av_diff(struct MPContext *mpctx, double offset)
     }
 }
 
-static void init_vo(struct MPContext *mpctx)
-{
-    struct MPOpts *opts = mpctx->opts;
-    struct vo_chain *vo_c = mpctx->vo_chain;
-
-#if HAVE_GPL
-    if (opts->gamma_gamma != 0)
-        video_set_colors(vo_c, "gamma", opts->gamma_gamma);
-    if (opts->gamma_brightness != 0)
-        video_set_colors(vo_c, "brightness", opts->gamma_brightness);
-    if (opts->gamma_contrast != 0)
-        video_set_colors(vo_c, "contrast", opts->gamma_contrast);
-    if (opts->gamma_saturation != 0)
-        video_set_colors(vo_c, "saturation", opts->gamma_saturation);
-    if (opts->gamma_hue != 0)
-        video_set_colors(vo_c, "hue", opts->gamma_hue);
-    video_set_colors(vo_c, "output-levels", opts->video_output_levels);
-#endif
-
-    mp_notify(mpctx, MPV_EVENT_VIDEO_RECONFIG, NULL);
-}
-
 double calc_average_frame_duration(struct MPContext *mpctx)
 {
     double total = 0;
@@ -1445,7 +1372,7 @@ void write_video(struct MPContext *mpctx)
             mpctx->error_playing = MPV_ERROR_VO_INIT_FAILED;
             goto error;
         }
-        init_vo(mpctx);
+        mp_notify(mpctx, MPV_EVENT_VIDEO_RECONFIG, NULL);
     }
 
     mpctx->time_frame -= get_relative_time(mpctx);

video/csputils.c
@@ -31,6 +31,7 @@
 
 #include "mp_image.h"
 #include "csputils.h"
+#include "options/m_config.h"
 #include "options/m_option.h"
 
 const struct m_opt_choice_alternatives mp_csp_names[] = {
@@ -94,15 +95,6 @@ const struct m_opt_choice_alternatives mp_csp_light_names[] = {
     {"709-1886",    MP_CSP_LIGHT_SCENE_709_1886},
     {"gamma1.2",    MP_CSP_LIGHT_SCENE_1_2},
     {0}
-};
-
-const char *const mp_csp_equalizer_names[MP_CSP_EQ_COUNT] = {
-    "brightness",
-    "contrast",
-    "hue",
-    "saturation",
-    "gamma",
-    "output-levels",
 };
 
 const struct m_opt_choice_alternatives mp_chroma_names[] = {
@@ -817,9 +809,25 @@ bool mp_colorspace_equal(struct mp_colorspace c1, struct mp_colorspace c2)
            c1.sig_peak == c2.sig_peak;
 }
 
+#define OPT_BASE_STRUCT struct mp_csp_equalizer_opts
+
+const struct m_sub_options mp_csp_equalizer_conf = {
+    .opts = (const m_option_t[]) {
+        OPT_INTRANGE("brightness", values[MP_CSP_EQ_BRIGHTNESS], 0, -100, 100),
+        OPT_INTRANGE("saturation", values[MP_CSP_EQ_SATURATION], 0, -100, 100),
+        OPT_INTRANGE("contrast", values[MP_CSP_EQ_CONTRAST], 0, -100, 100),
+        OPT_INTRANGE("hue", values[MP_CSP_EQ_HUE], 0, -100, 100),
+        OPT_INTRANGE("gamma", values[MP_CSP_EQ_GAMMA], 0, -100, 100),
+        OPT_CHOICE_C("video-output-levels", values[MP_CSP_EQ_OUTPUT_LEVELS], 0,
+                     mp_csp_levels_names),
+        {0}
+    },
+    .size = sizeof(struct mp_csp_equalizer_opts),
+};
+
 // Copy settings from eq into params.
 void mp_csp_copy_equalizer_values(struct mp_csp_params *params,
-                                  const struct mp_csp_equalizer *eq)
+                                  const struct mp_csp_equalizer_opts *eq)
 {
     params->brightness = eq->values[MP_CSP_EQ_BRIGHTNESS] / 100.0;
     params->contrast = (eq->values[MP_CSP_EQ_CONTRAST] + 100) / 100.0;
@@ -829,37 +837,28 @@ void mp_csp_copy_equalizer_values(struct mp_csp_params *params,
     params->levels_out = eq->values[MP_CSP_EQ_OUTPUT_LEVELS];
 }
 
-static int find_eq(int capabilities, const char *name)
+struct mp_csp_equalizer_state *mp_csp_equalizer_create(void *ta_parent,
+                                                    struct mpv_global *global)
 {
-    for (int i = 0; i < MP_CSP_EQ_COUNT; i++) {
-        if (strcmp(name, mp_csp_equalizer_names[i]) == 0)
-            return ((1 << i) & capabilities) ? i : -1;
-    }
-    return -1;
+    struct m_config_cache *c = m_config_cache_alloc(ta_parent, global,
+                                                    &mp_csp_equalizer_conf);
+    // The terrible, terrible truth.
+    return (struct mp_csp_equalizer_state *)c;
 }
 
-int mp_csp_equalizer_get(struct mp_csp_equalizer *eq, const char *property,
-                         int *out_value)
+bool mp_csp_equalizer_state_changed(struct mp_csp_equalizer_state *state)
 {
-    int index = find_eq(eq->capabilities, property);
-    if (index < 0)
-        return -1;
-
-    *out_value = eq->values[index];
-
-    return 0;
+    struct m_config_cache *c = (struct m_config_cache *)state;
+    return m_config_cache_update(c);
 }
 
-int mp_csp_equalizer_set(struct mp_csp_equalizer *eq, const char *property,
-                         int value)
+void mp_csp_equalizer_state_get(struct mp_csp_equalizer_state *state,
+                                struct mp_csp_params *params)
 {
-    int index = find_eq(eq->capabilities, property);
-    if (index < 0)
-        return 0;
-
-    eq->values[index] = value;
-
-    return 1;
+    struct m_config_cache *c = (struct m_config_cache *)state;
+    m_config_cache_update(c);
+    struct mp_csp_equalizer_opts *opts = c->opts;
+    mp_csp_copy_equalizer_values(params, opts);
 }
 
 void mp_invert_cmat(struct mp_cmat *out, struct mp_cmat *in)

video/csputils.h
@@ -185,6 +185,8 @@ enum mp_chroma_location {
 
 extern const struct m_opt_choice_alternatives mp_chroma_names[];
 
+extern const struct m_sub_options mp_csp_equalizer_conf;
+
 enum mp_csp_equalizer_param {
     MP_CSP_EQ_BRIGHTNESS,
     MP_CSP_EQ_CONTRAST,
@@ -195,27 +197,22 @@ enum mp_csp_equalizer_param {
     MP_CSP_EQ_COUNT,
 };
 
-#define MP_CSP_EQ_CAPS_COLORMATRIX \
-    ( (1 << MP_CSP_EQ_BRIGHTNESS) \
-    | (1 << MP_CSP_EQ_CONTRAST) \
-    | (1 << MP_CSP_EQ_HUE) \
-    | (1 << MP_CSP_EQ_SATURATION) \
-    | (1 << MP_CSP_EQ_OUTPUT_LEVELS) )
-
-#define MP_CSP_EQ_CAPS_GAMMA (1 << MP_CSP_EQ_GAMMA)
-#define MP_CSP_EQ_CAPS_BRIGHTNESS (1 << MP_CSP_EQ_BRIGHTNESS)
-
-extern const char *const mp_csp_equalizer_names[MP_CSP_EQ_COUNT];
-
 // Default initialization with 0 is enough, except for the capabilities field
-struct mp_csp_equalizer {
-    // Bit field of capabilities. For example (1 << MP_CSP_EQ_HUE) means hue
-    // support is available.
-    int capabilities;
+struct mp_csp_equalizer_opts {
     // Value for each property is in the range [-100, 100].
     // 0 is default, meaning neutral or no change.
     int values[MP_CSP_EQ_COUNT];
 };
+
+void mp_csp_copy_equalizer_values(struct mp_csp_params *params,
+                                  const struct mp_csp_equalizer_opts *eq);
+
+struct mpv_global;
+struct mp_csp_equalizer_state *mp_csp_equalizer_create(void *ta_parent,
+                                                    struct mpv_global *global);
+bool mp_csp_equalizer_state_changed(struct mp_csp_equalizer_state *state);
+void mp_csp_equalizer_state_get(struct mp_csp_equalizer_state *state,
+                                struct mp_csp_params *params);
 
 struct mp_csp_col_xy {
     float x, y;
@@ -232,13 +229,6 @@ static inline float mp_xy_Z(struct mp_csp_col_xy xy) {
 struct mp_csp_primaries {
     struct mp_csp_col_xy red, green, blue, white;
 };
-
-void mp_csp_copy_equalizer_values(struct mp_csp_params *params,
-                                  const struct mp_csp_equalizer *eq);
-int mp_csp_equalizer_set(struct mp_csp_equalizer *eq, const char *property,
-                         int value);
-int mp_csp_equalizer_get(struct mp_csp_equalizer *eq, const char *property,
-                         int *out_value);
 
 enum mp_csp avcol_spc_to_mp_csp(int avcolorspace);
 enum mp_csp_levels avcol_range_to_mp_csp_levels(int avrange);

video/filter/vf.h
@@ -141,15 +141,8 @@ struct vf_chain {
     void *wakeup_callback_ctx;
 };
 
-typedef struct vf_seteq {
-    const char *item;
-    int value;
-} vf_equalizer_t;
-
 enum vf_ctrl {
     VFCTRL_SEEK_RESET = 1,   // reset on picture and PTS discontinuities
-    VFCTRL_SET_EQUALIZER,    // set color options (brightness,contrast etc)
-    VFCTRL_GET_EQUALIZER,    // get color options (brightness,contrast etc)
 #if HAVE_GPL
     VFCTRL_SET_DEINTERLACE,  // Set deinterlacing status
     VFCTRL_GET_DEINTERLACE,  // Get deinterlacing status

video/filter/vf_scale.c
@@ -202,24 +202,6 @@ static struct mp_image *filter(struct vf_instance *vf, struct mp_image *mpi)
     return dmpi;
 }
 
-static int control(struct vf_instance *vf, int request, void *data)
-{
-    struct mp_sws_context *sws = vf->priv->sws;
-
-    switch (request) {
-    case VFCTRL_GET_EQUALIZER:
-        if (mp_sws_get_vf_equalizer(sws, data) < 1)
-            break;
-        return CONTROL_TRUE;
-    case VFCTRL_SET_EQUALIZER:
-        if (mp_sws_set_vf_equalizer(sws, data) < 1)
-            break;
-        return CONTROL_TRUE;
-    }
-
-    return CONTROL_UNKNOWN;
-}
-
 static int query_format(struct vf_instance *vf, unsigned int fmt)
 {
     if (IMGFMT_IS_HWACCEL(fmt) || sws_isSupportedInput(imgfmt2pixfmt(fmt)) < 1)
@@ -236,7 +218,6 @@ static int vf_open(vf_instance_t *vf)
     vf->reconfig = reconfig;
     vf->filter = filter;
     vf->query_format = query_format;
-    vf->control = control;
     vf->uninit = uninit;
     vf->priv->sws = mp_sws_alloc(vf);
     vf->priv->sws->log = vf->log;

commit 7ff4d11ab91f8219223112acfa10d3241c590bbf

command: restore OSD marker for video equalizer properties
Commit 03cf150ff3516 accidentally dropped these. Readd them in a simpler
way (so only a property_osd_display[] entry is enough). This commit
doesn't actually touch the video equalizer properties, because the
default value of 0 for the marker is what they require anyway.

options/m_property.h
@@ -82,11 +82,6 @@ enum mp_property_action {
     // Pass down an action to a sub-property.
     //  arg: struct m_property_action_arg*
     M_PROPERTY_KEY_ACTION,
-
-    // Get the (usually constant) value that indicates no change. Obscure
-    // special functionality for things like the volume property.
-    // Otherwise works like M_PROPERTY_GET.
-    M_PROPERTY_GET_NEUTRAL,
 };
 
 // Argument for M_PROPERTY_SWITCH

player/command.c
@@ -1805,9 +1805,6 @@ static int mp_property_volume(void *ctx, struct m_property *prop,
             .max = opts->softvol_max,
         };
         return M_PROPERTY_OK;
-    case M_PROPERTY_GET_NEUTRAL:
-        *(float *)arg = 100;
-        return M_PROPERTY_OK;
     case M_PROPERTY_PRINT:
         *(char **)arg = talloc_asprintf(NULL, "%i", (int)opts->softvol_volume);
         return M_PROPERTY_OK;
@@ -4301,6 +4298,8 @@ static const struct property_osd_display {
     int osd_progbar;
     // Needs special ways to display the new value (seeks are delayed)
     int seek_msg, seek_bar;
+    // Show a marker thing on OSD bar. Ignored if osd_progbar==0.
+    float marker;
     // Free-form message (if NULL, osd_name or the property name is used)
     const char *msg;
 } property_osd_display[] = {
@@ -4315,10 +4314,10 @@ static const struct property_osd_display {
     // audio
     { "volume", "Volume",
       .msg = "Volume: ${?volume:${volume}% ${?mute==yes:(Muted)}}${!volume:${volume}}",
-      .osd_progbar = OSD_VOLUME },
+      .osd_progbar = OSD_VOLUME, .marker = 100 },
     { "ao-volume", "AO Volume",
       .msg = "AO Volume: ${?ao-volume:${ao-volume}% ${?ao-mute==yes:(Muted)}}${!ao-volume:${ao-volume}}",
-      .osd_progbar = OSD_VOLUME },
+      .osd_progbar = OSD_VOLUME, .marker = 100 },
     { "mute", "Mute" },
     { "ao-mute", "AO Mute" },
     { "audio-delay", "A-V delay" },
@@ -4416,13 +4415,15 @@ static void show_property_osd(MPContext *mpctx, const char *name, int osd_mode)
     if ((osd_mode & MP_ON_OSD_BAR) && (prop.flags & CONF_RANGE) == CONF_RANGE) {
         if (prop.type == CONF_TYPE_INT) {
             int n = prop.min;
-            mp_property_do(name, M_PROPERTY_GET_NEUTRAL, &n, mpctx);
+            if (disp.osd_progbar)
+                n = disp.marker;
             int i;
             if (mp_property_do(name, M_PROPERTY_GET, &i, mpctx) > 0)
                 set_osd_bar(mpctx, disp.osd_progbar, prop.min, prop.max, n, i);
         } else if (prop.type == CONF_TYPE_FLOAT) {
             float n = prop.min;
-            mp_property_do(name, M_PROPERTY_GET_NEUTRAL, &n, mpctx);
+            if (disp.osd_progbar)
+                n = disp.marker;
             float f;
             if (mp_property_do(name, M_PROPERTY_GET, &f, mpctx) > 0)
                 set_osd_bar(mpctx, disp.osd_progbar, prop.min, prop.max, n, f);