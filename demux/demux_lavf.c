Su13n/mpv

commit 27fcd4ddc6cc76073795fbb48c55bfa2dce47063

demux_lavf: compensate timestamp resets for OGG web radio streams
Some OGG web radio streams use timestamp resets when a new song starts
(you can find those Xiph's directory - other streams there don't show
this behavior). Basically, the OGG stream behaves like concatenated OGG
files, and "of course" the timestamps will start at 0 again when the
song changes. This is very inconvenient, and breaks the seekable demuxer
cache. In fact, any kind of seeking will break

This is more time wasted in Xiph's bullshit. No, having timestamp resets
by design is not reasonable, and fuck you. I much prefer the awful
ICY/mp3 streaming mess, even if that's lower quality and awful. Maybe it
wouldn't be so bad if libavformat could tell us WHERE THE FUCK THE RESET
HAPPENS. But it doesn't, and the randomly changing timestamps is the
only thing we get from its API.

At this point, demux_lavf.c is like 90% hacks. But well, if libavformat
applies this strange mixture of being clever for us vs. giving us
unfiltered garbage (while pretending it abstracts everything, and hiding
_useful_ implementation/low level details), not much we can do.

This timestamp linearizing would, in general, probably be better done
after the decoder, because then we wouldn't need to deal with timestamp
resets. But the main purpose of this change is to fix seeking within the
demuxer cache, so we have to do it on the lowest level.

This can probably be applied to other containers and video streams too.
But that is untested. Some further caveats are explained in the manpage.

demux/demux_lavf.c
@@ -80,6 +80,7 @@ struct demux_lavf_opts {
     int hacks;
     char *sub_cp;
     int rtsp_transport;
+    int linearize_ts;
 };
 
 const struct m_sub_options demux_lavf_conf = {
@@ -103,6 +104,8 @@ const struct m_sub_options demux_lavf_conf = {
                 {"udp", 1},
                 {"tcp", 2},
                 {"http", 3})),
+        OPT_CHOICE("demuxer-lavf-linearize-timestamps", linearize_ts, 0,
+                   ({"no", 0}, {"auto", -1}, {"yes", 1})),
         {0}
     },
     .size = sizeof(struct demux_lavf_opts),
@@ -116,6 +119,7 @@ const struct m_sub_options demux_lavf_conf = {
         .probescore = AVPROBE_SCORE_MAX/4 + 1,
         .sub_cp = "auto",
         .rtsp_transport = 2,
+        .linearize_ts = -1,
     },
 };
 
@@ -136,7 +140,7 @@ struct format_hack {
     // Do not confuse player's position estimation (position is into external
     // segment, with e.g. HLS, player knows about the playlist main file only).
     bool clear_filepos : 1;
-    bool ignore_start : 1;
+    bool linearize_audio_ts : 1;// compensate timestamp resets (audio only)
     bool fix_editlists : 1;
     bool is_network : 1;
     bool no_seek : 1;
@@ -171,8 +175,9 @@ static const struct format_hack format_hacks[] = {
     {"h264", .if_flags = AVFMT_NOTIMESTAMPS },
     {"hevc", .if_flags = AVFMT_NOTIMESTAMPS },
 
-    // Rebasing start time to 0 is very weird with ogg shoutcast streams.
-    {"ogg", .ignore_start = true},
+    // Some Ogg shoutcast streams are essentially concatenated OGG files. They
+    // reset timestamps, which causes all sorts of problems.
+    {"ogg", .linearize_audio_ts = true},
 
     TEXTSUB("aqtitle"), TEXTSUB("jacosub"), TEXTSUB("microdvd"),
     TEXTSUB("mpl2"), TEXTSUB("mpsub"), TEXTSUB("pjs"), TEXTSUB("realtext"),
@@ -200,6 +205,9 @@ struct nested_stream {
 
 struct stream_info {
     struct sh_stream *sh;
+    double last_key_pts;
+    double highest_pts;
+    double ts_offset;
 };
 
 typedef struct lavf_priv {
@@ -225,6 +233,9 @@ typedef struct lavf_priv {
     bool pcm_seek_hack_disabled;
     AVStream *pcm_seek_hack;
     int pcm_seek_hack_packet_size;
+
+    int linearize_ts;
+    bool any_ts_fixed;
 
     // Proxying nested streams.
     struct nested_stream *nested;
@@ -527,6 +538,10 @@ static int lavf_check_file(demuxer_t *demuxer, enum demux_check check)
     if (lavfdopts->hacks)
         priv->avif_flags = priv->avif->flags | priv->format_hack.if_flags;
 
+    priv->linearize_ts = lavfdopts->linearize_ts;
+    if (priv->linearize_ts < 0 && !priv->format_hack.linearize_audio_ts)
+        priv->linearize_ts = 0;
+
     demuxer->filetype = priv->avif->name;
 
     if (priv->format_hack.detect_charset)
@@ -690,6 +705,9 @@ static void handle_new_stream(demuxer_t *demuxer, int i)
             // A real video stream probably means it's a packet based format.
             priv->pcm_seek_hack_disabled = true;
             priv->pcm_seek_hack = NULL;
+            // Also, we don't want to do this shit for ogv videos.
+            if (priv->linearize_ts < 0)
+                priv->linearize_ts = 0;
         }
 
         sh->codec->disp_w = codec->width;
@@ -760,6 +778,8 @@ static void handle_new_stream(demuxer_t *demuxer, int i)
     struct stream_info *info = talloc_zero(priv, struct stream_info);
     *info = (struct stream_info){
         .sh = sh,
+        .last_key_pts = MP_NOPTS_VALUE,
+        .highest_pts = MP_NOPTS_VALUE,
     };
     assert(priv->num_streams == i); // directly mapped
     MP_TARRAY_APPEND(priv, priv->streams, priv->num_streams, info);
@@ -1050,7 +1070,7 @@ static int demux_open_lavf(demuxer_t *demuxer, enum demux_check check)
     demuxer->ts_resets_possible =
         priv->avif_flags & (AVFMT_TS_DISCONT | AVFMT_NOTIMESTAMPS);
 
-    if (avfc->start_time != AV_NOPTS_VALUE && !priv->format_hack.ignore_start)
+    if (avfc->start_time != AV_NOPTS_VALUE)
         demuxer->start_time = avfc->start_time / (double)AV_TIME_BASE;
 
     demuxer->fully_read = priv->format_hack.fully_read;
@@ -1136,7 +1156,8 @@ static bool demux_lavf_read_packet(struct demuxer *demux,
     update_metadata(demux);
 
     assert(pkt->stream_index >= 0 && pkt->stream_index < priv->num_streams);
-    struct sh_stream *stream = priv->streams[pkt->stream_index]->sh;
+    struct stream_info *info = priv->streams[pkt->stream_index];
+    struct sh_stream *stream = info->sh;
     AVStream *st = priv->avfc->streams[pkt->stream_index];
 
     if (!demux_stream_is_selected(stream)) {
@@ -1169,6 +1190,30 @@ static bool demux_lavf_read_packet(struct demuxer *demux,
 
     dp->stream = stream->index;
 
+    if (priv->linearize_ts) {
+        dp->pts = MP_ADD_PTS(dp->pts, info->ts_offset);
+        dp->dts = MP_ADD_PTS(dp->dts, info->ts_offset);
+
+        double pts = MP_PTS_OR_DEF(dp->pts, dp->dts);
+        if (pts != MP_NOPTS_VALUE) {
+            if (dp->keyframe) {
+                if (pts < info->highest_pts) {
+                    MP_WARN(demux, "Linearizing discontinuity: %f -> %f\n",
+                            pts, info->highest_pts);
+                    // Note: introduces a small discontinuity by a frame size.
+                    double diff = info->highest_pts - pts;
+                    dp->pts = MP_ADD_PTS(dp->pts, diff);
+                    dp->dts = MP_ADD_PTS(dp->dts, diff);
+                    pts += diff;
+                    info->ts_offset += diff;
+                    priv->any_ts_fixed = true;
+                }
+                info->last_key_pts = pts;
+            }
+            info->highest_pts = MP_PTS_MAX(info->highest_pts, pts);
+        }
+    }
+
     *mp_pkt = dp;
     return true;
 }
@@ -1185,6 +1230,14 @@ static void demux_seek_lavf(demuxer_t *demuxer, double seek_pts, int flags)
             seek_pts = seek_pts * demuxer->duration;
         stream_control(priv->stream, STREAM_CTRL_OPTICAL_CRAP_HACK2, &seek_pts);
         return;
+    }
+
+    if (priv->any_ts_fixed)  {
+        // helpful message to piss of users
+        MP_WARN(demuxer, "Some timestamps returned by the demuxer were linearized. "
+                         "A low level seek was requested; this won't work due to "
+                         "restrictions in libavformat's API. You may have more "
+                         "luck by enabling or enlarging the mpv cache.\n");
     }
 
     if (!(flags & SEEK_FORWARD))

commit 682697150c9868868bc44700158ec22fbe8d1bdb

demux_lavf: fix seeking in ogg audio streams
This detected the first packet demuxed after a seek as timestamp
discontinuity. Obviously this is non-sense. Since the OGG radio streams
for which this feature was introduced are normally unseekable, it's
simple to fix this: simply disable it (if in auto mode, the default) as
soon as a seek is performed. This code is never called if the stream is
considered unseekable, unless the user forced it.

There's still a chance this linearization is performed before a seek
happens. This will be a bit awkward, but no worse than without this
feature, since seeking with timestamp resets is inherently broken in
both mpv and libavformat.

Fixes: #6974
Fixes: 27fcd4d

demux/demux_lavf.c
@@ -1213,6 +1213,9 @@ static void demux_seek_lavf(demuxer_t *demuxer, double seek_pts, int flags)
                          "luck by enabling or enlarging the mpv cache.\n");
     }
 
+    if (priv->linearize_ts < 0)
+        priv->linearize_ts = 0;
+
     if (!(flags & SEEK_FORWARD))
         avsflags = AVSEEK_FLAG_BACKWARD;
 