Su13n/mpv

commit c3694f0acb7f71daac7606fafbadcb7b500ca35e

path: do not use old_home for win32 exe dir
Apparently mpv supports loading config files from the same directory as
the mpv.exe. This is a fallback of some sort. It used the old_home
mechanism.

I want to add a warning if old_home exists, but that would always show
the warning on win32. Obviously we don't want that.

Add a separate exe_dir entry to deal with that.

Untested, but probably works.

options/path.c
@@ -59,6 +59,7 @@ static const char *const config_dirs[] = {
     "home",
     "old_home",
     "osxbundle",
+    "exe_dir"
     "global",
 };
 

osdep/path-win.c
@@ -90,7 +90,7 @@ const char *mp_get_platform_path_win(void *talloc_ctx, const char *type)
     } else {
         if (strcmp(type, "home") == 0)
             return mp_get_win_app_dir(talloc_ctx);
-        if (strcmp(type, "old_home") == 0)
+        if (strcmp(type, "exe_dir") == 0)
             return mp_get_win_exe_dir(talloc_ctx);
         // Not really true, but serves as a way to return a lowest-priority dir.
         if (strcmp(type, "global") == 0)

commit 59577bd73da215a3e0fdd382ce01f448694845fa

path: fix broken exe-dir[/mpv] config locations
This is a regression since c3694f0, at least on Windows.

Fixes #7889
Fixes #7881

options/path.c
@@ -59,7 +59,7 @@ static const char *const config_dirs[] = {
     "home",
     "old_home",
     "osxbundle",
-    "exe_dir"
+    "exe_dir",
     "global",
 };
 