Su13n/mpv

commit f9dc695b580c394bf4f9833d36e91b7fcbe009ea

vo_gpu_next: delay swapchain submit until flip_frame()
Presents frames at the correct time when DS is disabled.

video/out/vo_gpu_next.c
@@ -1063,14 +1063,17 @@ done:
     if (!valid) // clear with purple to indicate error
         pl_tex_clear(gpu, swframe.fbo, (float[4]){ 0.5, 0.0, 1.0, 1.0 });
 
-    if (!pl_swapchain_submit_frame(p->sw))
-        MP_ERR(vo, "Failed presenting frame!\n");
+    pl_gpu_flush(gpu);
 }
 
 static void flip_page(struct vo *vo)
 {
     struct priv *p = vo->priv;
     struct ra_swapchain *sw = p->ra_ctx->swapchain;
+
+    if (!pl_swapchain_submit_frame(p->sw))
+        MP_ERR(vo, "Failed presenting frame!\n");
+
     sw->fns->swap_buffers(sw);
 }
 

commit 2f19b01d0da0d0825a32e07a3093e08a2212123e

vo_gpu_next: fix assertion on wayland
This code failed to handle the case of the swapchain submission being
skipped because the window was invisible.

Fixes: f9dc695b580c394bf4f9833d36e91b7fcbe009ea

video/out/vo_gpu_next.c
@@ -116,6 +116,7 @@ struct priv {
     double last_pts;
     bool is_interpolated;
     bool want_reset;
+    bool frame_pending;
 
     struct m_config_cache *opts_cache;
     struct mp_csp_equalizer_state *video_eq;
@@ -1064,6 +1065,7 @@ done:
         pl_tex_clear(gpu, swframe.fbo, (float[4]){ 0.5, 0.0, 1.0, 1.0 });
 
     pl_gpu_flush(gpu);
+    p->frame_pending = true;
 }
 
 static void flip_page(struct vo *vo)
@@ -1071,8 +1073,11 @@ static void flip_page(struct vo *vo)
     struct priv *p = vo->priv;
     struct ra_swapchain *sw = p->ra_ctx->swapchain;
 
-    if (!pl_swapchain_submit_frame(p->sw))
-        MP_ERR(vo, "Failed presenting frame!\n");
+    if (p->frame_pending) {
+        if (!pl_swapchain_submit_frame(p->sw))
+            MP_ERR(vo, "Failed presenting frame!\n");
+        p->frame_pending = false;
+    }
 
     sw->fns->swap_buffers(sw);
 }