Su13n/mpv

commit 4b691641472b487831a0b0f7637d5355d79387cd

x11_common: prefer Xft.dpi for HiDPI scaling
Xft.dpi is much more widely used nowadays by GUI programs compared to
the X11 screen DPI.
This is the best we can get for a vendor-neutral scaling preference
value under X11 in terms of adoption.
If Xft.dpi isn't available, the X11 screen DPI is used as a fallback.

video/out/x11_common.c
@@ -29,6 +29,7 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/Xatom.h>
+#include <X11/Xresource.h>
 #include <X11/keysym.h>
 #include <X11/XKBlib.h>
 #include <X11/XF86keysym.h>
@@ -626,6 +627,44 @@ static void vo_x11_get_x11_screen_dpi_scale(struct vo_x11_state *x11)
     }
 }
 
+// Get the dpi scale from the Xft.dpi resource. In practice, this value is much more
+// commonly used by GUI programs for scaling compared to the x11 screen dpi.
+// This is always a preference value so it's also more consistent.
+static bool vo_x11_get_xft_dpi_scale(struct vo_x11_state *x11)
+{
+    XrmInitialize();
+    char *resman = XResourceManagerString(x11->display);
+    if (!resman)
+        return false;
+
+    XrmDatabase db = XrmGetStringDatabase(resman);
+    if (!db)
+        return false;
+
+    XrmValue ret;
+    char *type;
+    double base_dpi = 96;
+    bool success = false;
+    if (XrmGetResource(db, "Xft.dpi", "String", &type, &ret) == True &&
+        ret.addr && !strcmp("String", type))
+    {
+        char *end;
+        long value = strtol(ret.addr, &end, 10);
+        if (*ret.addr && *end == '\0') {
+            int s = lrint(MPCLAMP(2 * value / base_dpi, 0, 20));
+            if (s > 2 && s < 20) {
+                x11->dpi_scale = s / 2.0;
+                MP_VERBOSE(x11, "Using Xft.dpi scale %g for prescaling. This can "
+                           "be disabled with --hidpi-window-scale=no.\n",
+                           x11->dpi_scale);
+                success = true;
+            }
+        }
+    }
+    XrmDestroyDatabase(db);
+    return success;
+}
+
 bool vo_x11_init(struct vo *vo)
 {
     char *dispName;
@@ -690,7 +729,8 @@ bool vo_x11_init(struct vo *vo)
            x11->ws_width, x11->ws_height, dispName,
            x11->display_is_local ? "local" : "remote");
 
-    vo_x11_get_x11_screen_dpi_scale(x11);
+    if (!vo_x11_get_xft_dpi_scale(x11))
+        vo_x11_get_x11_screen_dpi_scale(x11);
 
     x11->wm_type = vo_wm_detect(vo);
 

commit 355ebf11aac55d6a35b2928b9c3ae6f75fe558c9

x11_common: fix Xft.dpi detection ignoring --hidpi-window-scale
closes #13466
fixes: 4b691641472b487831a0b0f7637d5355d79387cd

video/out/x11_common.c
@@ -617,7 +617,7 @@ static void vo_x11_get_x11_screen_dpi_scale(struct vo_x11_state *x11)
     double dpi_x = x11->ws_width * 25.4 / w_mm;
     double dpi_y = x11->ws_height * 25.4 / h_mm;
     double base_dpi = 96;
-    if (isfinite(dpi_x) && isfinite(dpi_y) && x11->opts->hidpi_window_scale) {
+    if (isfinite(dpi_x) && isfinite(dpi_y)) {
         int s_x = lrint(MPCLAMP(2 * dpi_x / base_dpi, 0, 20));
         int s_y = lrint(MPCLAMP(2 * dpi_y / base_dpi, 0, 20));
         if (s_x == s_y && s_x > 2 && s_x < 20) {
@@ -731,8 +731,10 @@ bool vo_x11_init(struct vo *vo)
            x11->ws_width, x11->ws_height, dispName,
            x11->display_is_local ? "local" : "remote");
 
-    if (!vo_x11_get_xft_dpi_scale(x11))
-        vo_x11_get_x11_screen_dpi_scale(x11);
+    if (x11->opts->hidpi_window_scale) {
+        if (!vo_x11_get_xft_dpi_scale(x11))
+            vo_x11_get_x11_screen_dpi_scale(x11);
+    }
 
     x11->wm_type = vo_wm_detect(vo);
 