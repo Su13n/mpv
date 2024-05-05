Su13n/mpv

commit b80e4e46a3e04c0aa4fea84ff57c11551772c4de

osc: show left arrow when paused while playing backwards
Use existing glyph for playlist buttons to show a triangle
pointing left if playback is stopped with a backward playback
direction.

player/lua/osc.lua
@@ -1878,7 +1878,11 @@ function osc_init()
 
     ne.content = function ()
         if mp.get_property("pause") == "yes" then
-            return ("\238\132\129")
+            if mp.get_property("play-direction", "forward") ~= "backward" then 
+                return ("\238\132\129")
+            else
+                return ("\238\132\144")
+            end
         else
             return ("\238\128\130")
         end

commit dd9b2fb99c29986bacae49c703a8324faba58ac9

osc: remove trailing space
Fixes: b80e4e46a3e04c0aa4fea84ff57c11551772c4de

player/lua/osc.lua
@@ -1878,7 +1878,7 @@ function osc_init()
 
     ne.content = function ()
         if mp.get_property("pause") == "yes" then
-            if mp.get_property("play-direction", "forward") ~= "backward" then 
+            if mp.get_property("play-direction", "forward") ~= "backward" then
                 return ("\238\132\129")
             else
                 return ("\238\132\144")